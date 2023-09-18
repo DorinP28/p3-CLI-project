141784cdb2282a116e7632c899b207a36a50dcdf
Dorin Porcereanu <porcereanu.dorin@gmail.com>
2023-09-18 05:15:15 -0400

    helpers.py handle borrowed book

--- lib/helpers.py
+++ lib/helpers.py
@@ -12,3 +12,21 @@ def show_available_books():
         print("\nAvailable Books:")
         for book in available_books:
             print(f"ID: {book.id}, Name: {book.name}, Author: {book.author.name}")
+
+
+def handle_borrow_book(user):
+    show_available_books()
+    book_id = input("Enter the ID of the book you want to borrow: ")
+    book = (
+        session.query(Book)
+        .filter(Book.id == book_id, Book.is_available == True)
+        .first()
+    )
+    if book:
+        book.is_available = False
+        borrowed_book = BorrowedBook(user_id=user.id, book_id=book.id)
+        session.add(borrowed_book)
+        session.commit()
+        print(f"\nYou've borrowed {book.name}!")
+    else:
+        print("\nInvalid choice or book not available.")
