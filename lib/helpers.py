b568232d9b12bee53005d8002115a11b99fa6be2
Dorin Porcereanu <porcereanu.dorin@gmail.com>
2023-09-18 04:35:21 -0400

    helpers.py show available books

--- lib/helpers.py
+++ lib/helpers.py
@@ -1,2 +1,12 @@
 from db.models import Book
 from db.session import Session
+
+session = Session()
+
+
+def show_available_books():
+    available_books = session.query(Book).filter(Book.is_available == True).all()
+    if not available_books:
+        print("\nNo books available at the moment.")
+    else:
+        print("\nAvailable Books:")
