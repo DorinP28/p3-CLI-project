e7649ed81ed6354274bf7e5090b321183279d597
Dorin Porcereanu <porcereanu.dorin@gmail.com>
2023-09-18 04:38:52 -0400

    seed.py faker implement

--- lib/seed.py
+++ lib/seed.py
@@ -1,5 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from db.models import Base, Author, Book, User
+from faker import Faker
 
 engine = create_engine("sqlite:///library.db")
+
+fake = Faker()
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
